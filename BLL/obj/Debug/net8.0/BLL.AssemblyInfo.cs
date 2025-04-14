0b524f242fd95ec70d1ea540434be679f2df1759
yousseff2255 <yousseff2255@gmail.com>
2025-04-14 19:05:37 +0200

    DTOs

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d58e274a86730a3aba2abd0968efe3d220cd618")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c360f93b9be53c398bcbb2a48d6c8c211077598")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

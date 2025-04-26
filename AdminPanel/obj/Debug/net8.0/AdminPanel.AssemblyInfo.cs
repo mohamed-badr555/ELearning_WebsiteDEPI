a3f5faf67b65ded99b9f3b5182e7ac384c291236
aymanyousef890 <91222918+aymanyousef890@users.noreply.github.com>
2025-04-26 19:13:57 +0300

    add course view

--- AdminPanel/obj/Debug/net8.0/AdminPanel.AssemblyInfo.cs
+++ AdminPanel/obj/Debug/net8.0/AdminPanel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec63f04908e643ffcb389e2261d976f7087072d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5082e434ef3205b330110c3ea75414dcee58084d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminPanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
